8fd8dc129a9c554df5ad34651d5dfd3bcf321d8f
Glenn Larsen <50579947+glennlarsen@users.noreply.github.com>
2022-03-30 20:35:41 +0200

    remove event listener contact

--- js/contact.js
+++ js/contact.js
@@ -2,8 +2,3 @@ import navigation from "./components/navigation.js";
 
 navigation();
 
-document.addEventListener("DOMContentLoaded", () => {
-    let w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-    let h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-    $("html, body").css({"width":w,"height":h});
-});
