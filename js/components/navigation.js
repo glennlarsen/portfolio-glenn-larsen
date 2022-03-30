fe41323646d3f4f3ac8b39ffd5aa926e931f9da9
Glenn Larsen <50579947+glennlarsen@users.noreply.github.com>
2022-03-30 20:38:42 +0200

    Update navigation.js

--- js/components/navigation.js
+++ js/components/navigation.js
@@ -2,12 +2,6 @@
 
 export default function navigation() {
 
-    document.addEventListener("DOMContentLoaded", () => {
-        let w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-        let h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-        $("html, body").css({"width":w,"height":h});
-    });
-
     const { pathname } = document.location;
     const navigation = document.querySelector(".navigation");
 
